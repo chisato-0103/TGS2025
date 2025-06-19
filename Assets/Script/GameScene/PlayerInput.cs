0cc2821916f1a1634755308fe1110b3d9cd01ed2
chisato <x24014xx@aitech.ac.jp>
2025-06-19 17:01:02 +0900

    - オスゴリラをクリックしたらスコア減点（unityエディタから数値変更可能） -スポーンさせたゴリラを3秒立ったら自然消滅（こちらもunityエディタから数値の変更可能です）

--- Assets/Script/GameScene/PlayerInput.cs
+++ Assets/Script/GameScene/PlayerInput.cs
@@ -1,13 +1,11 @@
-// PlayerInput.cs
-
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // [追加] GameManagerへの参照を保持する変数
+    // GameManagerへの参照を保持する変数
     private GameManager gameManager;
 
-    // [追加] StartメソッドでGameManagerを一度だけ探して保持しておく
+    // StartメソッドでGameManagerを一度だけ探して保持しておく
     void Start()
     {
         // シーン内からGameManagerコンポーネントを探してくる
@@ -26,11 +24,11 @@ public class PlayerInput : MonoBehaviour
                 Target target = hit.collider.GetComponent<Target>();
                 if (target != null)
                 {
-                    // [追加] 的を破壊する直前に、スコアを加算する処理を呼び出す
+                    // 的を破壊する直前に、スコアを加算する処理を呼び出す
                     // とりあえず10点加算する
                     if (gameManager != null)
                     {
-                        gameManager.AddScore(10);
+                        gameManager.AddScore(target.points);
                     }
 
                     target.Hit();
