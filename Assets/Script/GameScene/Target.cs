064b36a7d47760752dded6684f6a087d2482d229
chisato <x24014xx@aitech.ac.jp>
2025-06-19 17:01:02 +0900

    - オスゴリラをクリックしたらスコア減点（unityエディタから数値変更可能） -スポーンさせたゴリラを3秒立ったら自然消滅（こちらもunityエディタから数値の変更可能です）

--- Assets/Script/GameScene/Target.cs
+++ Assets/Script/GameScene/Target.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    //この的の点数（Inspectorから変更可能）
+    [SerializeField]
+    public int points = 10;
+
+    //この的が自動で消えるまでの時間（秒）
+    [SerializeField]
+    private float lifetime = 3.0f;
+
+    //オブジェクトが生成された時に一度だけ呼ばれる
+    void Start()
+    {
+        // lifetime秒後に、このゲームオブジェクトを破棄する
+        Destroy(gameObject, lifetime);
+    }
+
     // このメソッドが外部から呼び出されたら、オブジェクトを破壊する
     public void Hit()
     {
