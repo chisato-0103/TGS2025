cfcdb619244a81eb25c472d0d635ffef304bebc6
ArusikaT <sougo.tgw@gmail.com>
2025-06-23 17:22:23 +0900

    ランキングで使うクラスを完成。またリザルト画面で使う画像を追加。

--- Assets/Script/RankingScript/RankingData.cs
+++ Assets/Script/RankingScript/RankingData.cs
@@ -2,12 +2,39 @@ using UnityEngine;
 
 public class RankingData
 {
-    /*
     GameManager gm = new GameManager();
-    public int score;
+    public int score = 0;
+    public int[] rscore = new int[10];
+    public string[] Pname = new string[10];
     public RankingData(GameManager gm)
     {
         this.gm = gm;
-        this.score = gm.GetScore();
-    }*/
+        this.score = gm.getCurrentScore();
+        for (int i = 0; i < 10; i++)
+        {
+            rscore[i] = 0;
+            Pname[i] = "NoData";
+        }
+    }
+
+    public void RankingChange()
+    {
+        this.score = gm.getCurrentScore();
+        if (score > rscore[9])
+        {
+            rscore[9] = score;
+            for (int i = 8; i >= 0; i--)
+            {
+                if (score < rscore[i])
+                {
+                    break;
+                }
+                else
+                {
+                    rscore[i + 1] = rscore[i];
+                    rscore[i] = score;
+                }
+            }
+        }
+    }
 }
