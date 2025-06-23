03fe0b37f6c8b8b81d68b6204a65bc672865c5da
ArusikaT <sougo.tgw@gmail.com>
2025-06-23 14:36:09 +0900

    ランキングプログラムを追加。プレイヤー名表示は一旦保留。スコアを持ってくる部分に警告が出ており対応を考え中

---
+++ Assets/Script/RankingScript/Ranking.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+public class Ranking : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameManager gm = new GameManager();
+    public TextMeshProUGUI RankingText;
+    void Start()
+    {
+        int score = gm.getCurrentScore();
+        for (int i = 0; i < 10; i++)
+        {
+            RankingText.text += "Ranking:" + (i + 1) + "    Score:" + score + "\n";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
