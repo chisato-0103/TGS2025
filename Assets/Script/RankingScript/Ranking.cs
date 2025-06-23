5207d77fc967e5baa3946daf6f1e28238ef906e7
ArusikaT <sougo.tgw@gmail.com>
2025-06-23 17:22:23 +0900

    ランキングで使うクラスを完成。またリザルト画面で使う画像を追加。

--- Assets/Script/RankingScript/Ranking.cs
+++ Assets/Script/RankingScript/Ranking.cs
@@ -5,14 +5,27 @@ public class Ranking : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     GameManager gm = new GameManager();
+    RankingData rd;
     public TextMeshProUGUI RankingText;
+    void Awake()
+    {
+        rd = new RankingData(gm);
+    }
     void Start()
     {
+        rd.RankingChange();
         RankingText.text = "";
-        int score = gm.getCurrentScore();
+        //int score = gm.getCurrentScore();
         for (int i = 0; i < 10; i++)
         {
-            RankingText.text += "Ranking:" + (i + 1) + "    Score:" + score + "\n";
+            if (i + 1 >= 10)
+            {
+                RankingText.text += "Ranking:" + (i + 1) + "    PlayerName:" + rd.Pname[i] + "    Score:" + rd.rscore[i] + "\n";
+            }
+            else
+            {
+                RankingText.text += "Ranking:" + (i + 1) + "      PlayerName:" + rd.Pname[i] + "    Score:" + rd.rscore[i] + "\n";
+            }
         }
     }
 
