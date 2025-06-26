a21dc58ead3be6bbc7717135d50b1f6d77fd6d1e
ArusikaT <sougo.tgw@gmail.com>
2025-06-26 19:09:18 +0900

    ランキング画面のプログラミングの完成(今回はシーンの結合がされてないため点数はランダムで入れてる)。またゲームマネージャースクリプトのcurrentScoreをstatic化した(シーン結合ができたら他の方法も行けるようになるかも)。リザルトシーンについては特に変更はなし。

--- Assets/Script/ResultScript/GoriraImage.cs
+++ Assets/Script/ResultScript/GoriraImage.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 public class GoriraImage : MonoBehaviour
 {
     Score sr;
-    public Image ResultImage;          // 差し替え対象のImage
-    public Sprite loseGorira;        // スコアが低いときの画像
-    public Sprite Gorira;        // 中間
+    public Image ResultImage;         // 差し替え対象のImage
+    public Sprite loseGorira;         // スコアが低いときの画像
+    public Sprite Gorira;             // 中間
     public Sprite WinGorira;          // 高スコア
 
     float time = 0.0f;
@@ -14,15 +14,13 @@ public class GoriraImage : MonoBehaviour
     bool flag = false;
     void Start()
     {
-        sr = FindObjectOfType<Score>();
-        //ResultImage.gameObject.SetActive(false);
+        sr = FindFirstObjectByType<Score>();
 
     }
     void Update()
     {
         if (time >= 4.0 && flag == false)
         {
-            //ResultImage.gameObject.SetActive(true);
             // スコアに応じて画像を変更
             if (sr.score >= 800.0f)
             {
