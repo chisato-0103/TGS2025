815bfa4c57f05f6ae0f5668ef515d6c576747c57
ArusikaT <sougo.tgw@gmail.com>
2025-07-03 17:05:40 +0900

    テキストボックスを新しくしたのでそれに合わせてプログラミングを変えました。(古い方と新しい方は両方存在しています)

---
+++ Assets/Script/RankingScript/RankingDataOld.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class RankingDataOld
+{
+    public int score = 0;
+    public int[] rscore = new int[10];
+    public string[] Pname = new string[10];
+    public RankingDataOld()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            rscore[i] = 0;
+            Pname[i] = "データなし";
+        }
+    }
+
+    public void RankingChange()
+    {
+        this.score = GameManager.currentScore;
+        //this.score = Random.Range(0, 10);
+        if (score > rscore[9])
+        {
+            rscore[9] = score;
+            for (int i = 8; i >= 0; i--)
+            {
+                if (score < rscore[i])
+                {
+                    break;
+                }
+                else
+                {
+                    rscore[i + 1] = rscore[i];
+                    rscore[i] = score;
+                }
+            }
+        }
+    }
+}
