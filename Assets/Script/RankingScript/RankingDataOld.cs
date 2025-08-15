edcd7d7b691d2f8745786cc62575776d82b5f2d2
ArusikaT <sougo.tgw@gmail.com>
2025-08-16 01:21:48 +0900

    ランキング画面のイラストの差し替え、またゲームシーンの変更に合わせ得点表記から匹数表記に変更しました

--- Assets/Script/RankingScript/RankingDataOld.cs
+++ Assets/Script/RankingScript/RankingDataOld.cs
@@ -3,17 +3,15 @@ using UnityEngine;
 public class RankingDataOld
 {
     public int score = 0;
-    public static int[] rscore = new int[10];
-    public static string[] Pname = new string[10];
+    public static int[] rscore = new int[5];
     public static bool TitleFlag = false;
     public RankingDataOld()
     {
         if (TitleFlag == false)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
                 rscore[i] = 0;
-                Pname[i] = "データなし";
             }
             TitleFlag = true;
         }
@@ -24,11 +22,11 @@ public class RankingDataOld
     {
         this.score = GameManager.currentScore;
         //this.score = Random.Range(0, 10);
-        if (score > rscore[9])
+        if (score > rscore[4])
         {
             int i;
             // スコアより小さいものを後ろにずらす
-            for (i = 8; i >= 0 && rscore[i] < score; i--)
+            for (i = 3; i >= 0 && rscore[i] < score; i--)
             {
                 rscore[i + 1] = rscore[i];
             }
