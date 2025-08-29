442aa36ce036d04f9bfeab38d5b1c8d443adcf5a
chicakakehi <x24025xx@aitech.ac.jp>
2025-08-29 16:04:20 +0900

    確認

--- Assets/Script/Senser/M5StickReader.cs
+++ Assets/Script/Senser/M5StickReader.cs
@@ -161,7 +161,7 @@ public class M5StickReader : MonoBehaviour
             if (float.TryParse(lineY, out float y))
                 yaw = y;
 
-            throwPoint.x = -6.0f * yaw / 45f;
+            throwPoint.x = 6.0f * yaw / 45f;
         }
     }
 
@@ -197,7 +197,7 @@ public class M5StickReader : MonoBehaviour
     }
     public float getTarget_x()
     {
-        return -6 * yaw / 45.0f;
+        return 6 * yaw / 45.0f;
     }
     public bool getThrowActionflag()
     {
